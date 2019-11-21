c53f70052a82d902a836bbaa8df7b752a3483a2e
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2019-11-21 16:56:08 +0100

    Add Logs to Text watcher

--- app/src/main/java/com/widgets/edittextformatter/utils/FormatTextWatcher.java
+++ app/src/main/java/com/widgets/edittextformatter/utils/FormatTextWatcher.java
@@ -2,32 +2,36 @@ package com.widgets.edittextformatter.utils;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.widget.EditText;
 
 public class FormatTextWatcher implements TextWatcher {
+
+    private static final String TAG = FormatTextWatcher.class.getSimpleName();
+
     private EditText editText;
 
     public FormatTextWatcher(EditText editText) {
         this.editText = editText;
     }
 
-    public void init() {
+    void init() {
         editText.setText("");
         editText.setSelection(0);
     }
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
+        Log.d(TAG, "beforeTextChanged: s=" + s + ", start=" + start + ", count=" + count + ", after=" + after);
     }
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+        Log.d(TAG, "onTextChanged: s=" + s + ", start=" + start + ", before=" + before + ", count=" + count);
     }
 
     @Override
     public void afterTextChanged(Editable s) {
-
+        Log.d(TAG, "afterTextChanged: s=" + s);
     }
 }
