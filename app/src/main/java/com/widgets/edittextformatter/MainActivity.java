d2ef8f0dc93dcd4e7b7004acff98944be22bf28c
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2019-11-21 14:21:24 +0100

    Initial Commit

---
+++ app/src/main/java/com/widgets/edittextformatter/MainActivity.java
@@ -0,0 +1,14 @@
+package com.widgets.edittextformatter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
