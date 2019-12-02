4a564e14df96f2202c43b3b096ddc1524c4657d7
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2019-12-02 13:06:44 +0100

    Create FormatEditText and use it instead of Edit text

---
+++ app/src/main/java/com/widgets/edittextformatter/widgets/FormatEditText.java
@@ -0,0 +1,22 @@
+package com.widgets.edittextformatter.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+public class FormatEditText extends AppCompatEditText {
+
+
+    public FormatEditText(Context context) {
+        super(context);
+    }
+
+    public FormatEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FormatEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
