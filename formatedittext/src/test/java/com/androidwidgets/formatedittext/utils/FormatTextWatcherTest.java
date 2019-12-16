478adfc94e6391d7fb2880b33d25f4f1eedfcac6
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2019-12-16 22:11:41 +0100

    Move classes and test classes to library

--- formatedittext/src/test/java/com/androidwidgets/formatedittext/utils/FormatTextWatcherTest.java
+++ formatedittext/src/test/java/com/androidwidgets/formatedittext/utils/FormatTextWatcherTest.java
@@ -8,15 +8,12 @@ import com.androidwidgets.formatedittext.widgets.FormatEditText;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @RunWith(JUnitParamsRunner.class)
 public class FormatTextWatcherTest {
@@ -29,13 +26,13 @@ public class FormatTextWatcherTest {
 
     @Before
     public void setUp() {
-        editText = mock(FormatEditText.class);
-        formatter = mock(FormatTextWatcher.Formatter.class);
-        validator = mock(FormatTextWatcher.Validator.class);
-        listener = mock(FormatTextWatcher.ValidationListener.class);
+        editText = Mockito.mock(FormatEditText.class);
+        formatter = Mockito.mock(FormatTextWatcher.Formatter.class);
+        validator = Mockito.mock(FormatTextWatcher.Validator.class);
+        listener = Mockito.mock(FormatTextWatcher.ValidationListener.class);
 
         format = "----";
-        when(formatter.getFormat()).thenReturn(format);
+        Mockito.when(formatter.getFormat()).thenReturn(format);
     }
 
     @Test
@@ -44,8 +41,8 @@ public class FormatTextWatcherTest {
 
         textWatcher.init();
 
-        verify(editText).setText("");
-        verify(editText).setSelection(0);
+        Mockito.verify(editText).setText("");
+        Mockito.verify(editText).setSelection(0);
     }
 
     @Test
@@ -54,7 +51,7 @@ public class FormatTextWatcherTest {
 
         textWatcher.init();
 
-        verify(editText).setHint(format);
+        Mockito.verify(editText).setHint(format);
     }
 
     @Test
@@ -63,7 +60,7 @@ public class FormatTextWatcherTest {
 
         textWatcher.init();
 
-        verify(formatter).getFormat();
+        Mockito.verify(formatter).getFormat();
         assertEquals(format.length()+1, textWatcher.spyMaxLength);
     }
 
@@ -76,19 +73,19 @@ public class FormatTextWatcherTest {
         int formattedCursorPosition = 4;
         Result result = new Result(formattedUserInput, formattedCursorPosition);
 
-        when(editText.getSelectionStart()).thenReturn(currentCursorPosition);
-        when(formatter.format(userInput, currentCursorPosition)).thenReturn(result);
+        Mockito.when(editText.getSelectionStart()).thenReturn(currentCursorPosition);
+        Mockito.when(formatter.format(userInput, currentCursorPosition)).thenReturn(result);
         FormatTextWatcher textWatcher = new FormatTextWatcher(editText, formatter, validator, listener);
 
         // Act
-        Editable editable = mock(Editable.class);
-        when(editable.toString()).thenReturn(userInput);
+        Editable editable = Mockito.mock(Editable.class);
+        Mockito.when(editable.toString()).thenReturn(userInput);
         textWatcher.afterTextChanged(editable);
 
         // Assert
-        verify(formatter).format(userInput, currentCursorPosition);
-        verify(editText).setText(formattedUserInput);
-        verify(editText).setSelection(formattedCursorPosition);
+        Mockito.verify(formatter).format(userInput, currentCursorPosition);
+        Mockito.verify(editText).setText(formattedUserInput);
+        Mockito.verify(editText).setSelection(formattedCursorPosition);
     }
 
     @Test
@@ -97,7 +94,7 @@ public class FormatTextWatcherTest {
 
         textWatcher.init();
 
-        verify(listener).showEmpty();
+        Mockito.verify(listener).showEmpty();
     }
 
     @Test
@@ -110,17 +107,17 @@ public class FormatTextWatcherTest {
         int formattedCursorPosition = 4;
         Result result = new Result(formattedUserInput, formattedCursorPosition);
 
-        when(editText.getSelectionStart()).thenReturn(currentCursorPosition);
-        when(formatter.format(userInput, currentCursorPosition)).thenReturn(result);
+        Mockito.when(editText.getSelectionStart()).thenReturn(currentCursorPosition);
+        Mockito.when(formatter.format(userInput, currentCursorPosition)).thenReturn(result);
         FormatTextWatcher textWatcher = new FormatTextWatcher(editText, formatter, validator, listener);
 
         // Act
-        Editable editable = mock(Editable.class);
-        when(editable.toString()).thenReturn(userInput);
+        Editable editable = Mockito.mock(Editable.class);
+        Mockito.when(editable.toString()).thenReturn(userInput);
         textWatcher.afterTextChanged(editable);
 
         // Assert
-        verify(listener).showError();
+        Mockito.verify(listener).showError();
     }
 
     @Test
@@ -134,21 +131,21 @@ public class FormatTextWatcherTest {
         int formattedCursorPosition = 4;
         Result result = new Result(formattedUserInput, formattedCursorPosition);
 
-        when(formatter.getFormat()).thenReturn(format);
-        when(formatter.format(userInput, currentCursorPosition)).thenReturn(result);
-        when(formatter.removeFormat(formattedUserInput)).thenReturn(userInput);
-        when(editText.getSelectionStart()).thenReturn(currentCursorPosition);
-        when(validator.validate(formattedUserInput, userInput)).thenReturn(true);
+        Mockito.when(formatter.getFormat()).thenReturn(format);
+        Mockito.when(formatter.format(userInput, currentCursorPosition)).thenReturn(result);
+        Mockito.when(formatter.removeFormat(formattedUserInput)).thenReturn(userInput);
+        Mockito.when(editText.getSelectionStart()).thenReturn(currentCursorPosition);
+        Mockito.when(validator.validate(formattedUserInput, userInput)).thenReturn(true);
         FormatTextWatcher textWatcher = new FormatTextWatcher(editText, formatter, validator, listener);
 
         // Act
-        Editable editable = mock(Editable.class);
-        when(editable.toString()).thenReturn(userInput);
+        Editable editable = Mockito.mock(Editable.class);
+        Mockito.when(editable.toString()).thenReturn(userInput);
         textWatcher.afterTextChanged(editable);
 
         // Assert
-        verify(listener, times(0)).showError();
-        verify(listener).showSuccess();
+        Mockito.verify(listener, Mockito.times(0)).showError();
+        Mockito.verify(listener).showSuccess();
     }
 
     @Test
@@ -157,12 +154,12 @@ public class FormatTextWatcherTest {
         "$--$ | $  $, 1",
     })
     public void shouldPlaceTheCursorAtFirstPossiblePosition(String format, String initialText, int expectedCursorPosition) {
-        when(formatter.getFormat()).thenReturn(format);
+        Mockito.when(formatter.getFormat()).thenReturn(format);
         FormatTextWatcher textWatcher = new FormatTextWatcher(editText, formatter, validator, listener);
 
         textWatcher.setInitialText();
 
-        verify(editText).setText(initialText);
+        Mockito.verify(editText).setText(initialText);
     }
 
     @Test
@@ -171,15 +168,15 @@ public class FormatTextWatcherTest {
             "$$ -- $$, $$ 1  $$| $$ 1  $$"
     })
     public void shouldSetFormatWithoutDashesAsInitialTextWhenNoInputIsGiven(String format, String input, String expectedResult) {
-        when(formatter.getFormat()).thenReturn(format);
-        Editable editable = mock(Editable.class);
-        when(editable.toString()).thenReturn(input);
-        when(editText.getText()).thenReturn(editable);
+        Mockito.when(formatter.getFormat()).thenReturn(format);
+        Editable editable = Mockito.mock(Editable.class);
+        Mockito.when(editable.toString()).thenReturn(input);
+        Mockito.when(editText.getText()).thenReturn(editable);
         FormatTextWatcher textWatcher = new FormatTextWatcher(editText, formatter, validator, listener);
 
         textWatcher.setInitialTextWhenEmpty();
 
-        verify(editText).setText(expectedResult);
+        Mockito.verify(editText).setText(expectedResult);
     }
 
     @Test
@@ -190,14 +187,14 @@ public class FormatTextWatcherTest {
     })
     public void shouldPassFormattedInputToValidationListenerMethods(String format, String formattedInput, String expectedFormattedInput, String expectedUnformattedInput) {
         formatter = new DashFormatter(format);
-        Editable editable = mock(Editable.class);
-        when(editable.toString()).thenReturn(formattedInput);
-        when(editText.getText()).thenReturn(editable);
+        Editable editable = Mockito.mock(Editable.class);
+        Mockito.when(editable.toString()).thenReturn(formattedInput);
+        Mockito.when(editText.getText()).thenReturn(editable);
         FormatTextWatcher textWatcher = new FormatTextWatcher(editText, formatter, validator, listener);
 
         textWatcher.afterTextChanged(editable);
 
-        verify(validator).validate(expectedFormattedInput, expectedUnformattedInput);
+        Mockito.verify(validator).validate(expectedFormattedInput, expectedUnformattedInput);
     }
 
     @Test
@@ -209,21 +206,21 @@ public class FormatTextWatcherTest {
         int formattedCursorPosition = 4;
         Result result = new Result(formattedUserInput, formattedCursorPosition);
 
-        when(editText.getSelectionStart()).thenReturn(currentCursorPosition);
-        when(formatter.format(userInput, currentCursorPosition)).thenReturn(result);
+        Mockito.when(editText.getSelectionStart()).thenReturn(currentCursorPosition);
+        Mockito.when(formatter.format(userInput, currentCursorPosition)).thenReturn(result);
         FormatTextWatcher textWatcher = new FormatTextWatcher(editText, formatter, validator, listener);
 
         // Act
-        Editable editable = mock(Editable.class);
-        when(editable.toString()).thenReturn(userInput);
+        Editable editable = Mockito.mock(Editable.class);
+        Mockito.when(editable.toString()).thenReturn(userInput);
         textWatcher.afterTextChanged(editable);
 
         // Assert
-        verify(formatter).format(userInput, currentCursorPosition);
-        verify(editText).disableOnSelectionChange();
-        verify(editText).setText(formattedUserInput);
-        verify(editText).setSelection(formattedCursorPosition);
-        verify(editText).enableOnSelectionChange();
+        Mockito.verify(formatter).format(userInput, currentCursorPosition);
+        Mockito.verify(editText).disableOnSelectionChange();
+        Mockito.verify(editText).setText(formattedUserInput);
+        Mockito.verify(editText).setSelection(formattedCursorPosition);
+        Mockito.verify(editText).enableOnSelectionChange();
     }
 
     private class SpyFormatTextWatcher extends FormatTextWatcher {
