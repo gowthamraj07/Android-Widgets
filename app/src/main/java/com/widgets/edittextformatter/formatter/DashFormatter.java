c3732532b22cbca48d616eac5b57630731516a19
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2019-12-16 16:24:45 +0100

    Organize methods based on scope

--- app/src/main/java/com/widgets/edittextformatter/formatter/DashFormatter.java
+++ app/src/main/java/com/widgets/edittextformatter/formatter/DashFormatter.java
@@ -40,10 +40,6 @@ public class DashFormatter implements FormatTextWatcher.Formatter {
         return new Result(resultString, formattedCursorPosition);
     }
 
-    private int getFirstPossibleCursorPosition() {
-        return format.indexOf('-');
-    }
-
     @Override
     public String getFormat() {
         return format;
@@ -59,17 +55,6 @@ public class DashFormatter implements FormatTextWatcher.Formatter {
         return removeFormatFrom(userInput);
     }
 
-    private int getPossibleInputCount() {
-        int charCount = 0;
-        for (int index = 0; index < format.length(); index++) {
-            if (format.charAt(index) == '-') {
-                charCount++;
-            }
-        }
-
-        return charCount;
-    }
-
     private int calculateCursorPositionForUnformattedInput(final String input, final int currentCursorPosition) {
         int resultCursorPosition = currentCursorPosition;
         for (int index = 0; index < format.length() && index < input.length() && index < currentCursorPosition; index++) {
@@ -79,6 +64,21 @@ public class DashFormatter implements FormatTextWatcher.Formatter {
             }
         }
         return resultCursorPosition;
+    }
+
+    private int getFirstPossibleCursorPosition() {
+        return format.indexOf('-');
+    }
+
+    private int getPossibleInputCount() {
+        int charCount = 0;
+        for (int index = 0; index < format.length(); index++) {
+            if (format.charAt(index) == '-') {
+                charCount++;
+            }
+        }
+
+        return charCount;
     }
 
     private String removeFormatFrom(final String input) {
