a84425eff74a74d02a01544bdbba65c5ac25dae8
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2019-12-16 16:22:58 +0100

    Organize methods based on scope

--- app/src/main/java/com/widgets/edittextformatter/widgets/FormatEditText.java
+++ app/src/main/java/com/widgets/edittextformatter/widgets/FormatEditText.java
@@ -11,9 +11,8 @@ import com.widgets.edittextformatter.utils.FormatTextWatcher;
 
 public class FormatEditText extends AppCompatEditText {
 
-
-    private FormatTextWatcher.Formatter formatter;
     private boolean isOnSelectionChangeEnable;
+    private FormatTextWatcher.Formatter formatter;
     private FormatTextWatcher.Validator validator;
     private FormatTextWatcher.ValidationListener validationListener;
 
@@ -27,63 +26,6 @@ public class FormatEditText extends AppCompatEditText {
 
     public FormatEditText(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-    }
-
-    @Override
-    protected void onSelectionChanged(int selStart, int selEnd) {
-
-        if (selStart == selEnd && formatter != null && isOnSelectionChangeEnable) {
-            selStart = getStartSelection(selStart, formatter.getFormat(), getText().toString());
-            selStart = getLastSelection(selStart, formatter.getFormat(), getText().toString());
-
-            selEnd = selStart;
-
-            updateWhenCursorIsInInvalidPosition(selStart, selEnd);
-        }
-    }
-
-    private void updateWhenCursorIsInInvalidPosition(int selStart, int selEnd) {
-        boolean isCursorIsInInvalidPosition = selStart != -1;
-        if (isCursorIsInInvalidPosition) {
-            setSelection(selStart, selEnd);
-        }
-    }
-
-    public static int getStartSelection(int startSelection, String format, String input) {
-        int firstPossibleIndex = format.indexOf('-');
-        int lastPossibleCursorPosition = format.lastIndexOf('-') + 1;
-
-        if (input == null || input.isEmpty()) {
-            return 0;
-        }
-
-        int result = startSelection < firstPossibleIndex ? firstPossibleIndex : startSelection;
-        if (result > firstPossibleIndex && result < lastPossibleCursorPosition) {
-            return -1;
-        }
-
-        return result;
-    }
-
-    public static int getLastSelection(int startSelection, String format, String input) {
-        int firstPossibleIndex = format.indexOf('-');
-        int lastPossibleCursorPosition = format.lastIndexOf('-') + 1;
-
-        if (input == null || input.isEmpty()) {
-            return 0;
-        }
-
-        int result = startSelection > lastPossibleCursorPosition ? lastPossibleCursorPosition : startSelection;
-        if (result > firstPossibleIndex && result < lastPossibleCursorPosition) {
-            return -1;
-        }
-
-        return result;
-    }
-
-    public void initWith(FormatTextWatcher.Formatter formatter) {
-        this.formatter = formatter;
-        enableOnSelectionChange();
     }
 
     public void disableOnSelectionChange() {
@@ -119,5 +61,61 @@ public class FormatEditText extends AppCompatEditText {
 
     public void setValidationListener(FormatTextWatcher.ValidationListener validationListener) {
         this.validationListener = validationListener;
+    }
+
+    @Override
+    protected void onSelectionChanged(int selStart, int selEnd) {
+        if (selStart == selEnd && formatter != null && isOnSelectionChangeEnable) {
+            selStart = getStartSelection(selStart, formatter.getFormat(), getText().toString());
+            selStart = getLastSelection(selStart, formatter.getFormat(), getText().toString());
+
+            selEnd = selStart;
+
+            updateWhenCursorIsInInvalidPosition(selStart, selEnd);
+        }
+    }
+
+    static int getStartSelection(int startSelection, String format, String input) {
+        int firstPossibleIndex = format.indexOf('-');
+        int lastPossibleCursorPosition = format.lastIndexOf('-') + 1;
+
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        int result = startSelection < firstPossibleIndex ? firstPossibleIndex : startSelection;
+        if (result > firstPossibleIndex && result < lastPossibleCursorPosition) {
+            return -1;
+        }
+
+        return result;
+    }
+
+    static int getLastSelection(int startSelection, String format, String input) {
+        int firstPossibleIndex = format.indexOf('-');
+        int lastPossibleCursorPosition = format.lastIndexOf('-') + 1;
+
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        int result = startSelection > lastPossibleCursorPosition ? lastPossibleCursorPosition : startSelection;
+        if (result > firstPossibleIndex && result < lastPossibleCursorPosition) {
+            return -1;
+        }
+
+        return result;
+    }
+
+    private void updateWhenCursorIsInInvalidPosition(int selStart, int selEnd) {
+        boolean isCursorIsInInvalidPosition = selStart != -1;
+        if (isCursorIsInInvalidPosition) {
+            setSelection(selStart, selEnd);
+        }
+    }
+
+    private void initWith(FormatTextWatcher.Formatter formatter) {
+        this.formatter = formatter;
+        enableOnSelectionChange();
     }
 }
