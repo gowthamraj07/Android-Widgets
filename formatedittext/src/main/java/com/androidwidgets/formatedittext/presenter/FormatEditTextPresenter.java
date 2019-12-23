3f18436dfff2e408c1c2fc19167fdecb2d207fdc
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2019-12-23 05:52:31 +0100

    Make isOnSelectionChangeEnable instance field

--- formatedittext/src/main/java/com/androidwidgets/formatedittext/presenter/FormatEditTextPresenter.java
+++ formatedittext/src/main/java/com/androidwidgets/formatedittext/presenter/FormatEditTextPresenter.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class FormatEditTextPresenter {
-    private static boolean isOnSelectionChangeEnable;
+    private boolean isOnSelectionChangeEnable;
 
     public InputFilter[] addInputFilterTo(InputFilter[] existingFilters, InputFilter newFilter) {
         Set<InputFilter> filters = new HashSet<>(Arrays.asList(existingFilters));
@@ -54,7 +54,7 @@ public class FormatEditTextPresenter {
     }
 
     public void setIsOnSelectionChangeEnable(boolean isOnSelectionChangeEnable) {
-        FormatEditTextPresenter.isOnSelectionChangeEnable = isOnSelectionChangeEnable;
+        this.isOnSelectionChangeEnable = isOnSelectionChangeEnable;
     }
 
     public boolean isOnSelectionChangeEnable() {
