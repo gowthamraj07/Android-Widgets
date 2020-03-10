4253d3e3ee06b2034eb7bcafcbdef072a461841e
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2020-03-10 12:10:55 +0100

    Update package name for MainActivity

--- app/src/main/java/com/android/widgets/MainActivity.java
+++ app/src/main/java/com/android/widgets/MainActivity.java
@@ -1,4 +1,4 @@
-package com.widgets.edittextformatter;
+package com.android.widgets;
 
 import android.os.Bundle;
 
@@ -9,6 +9,7 @@ import com.androidwidgets.formatedittext.formatter.CurrencyFormatter;
 import com.androidwidgets.formatedittext.utils.FormatTextWatcher;
 import com.androidwidgets.formatedittext.widgets.FormatEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.widgets.edittextformatter.R;
 
 public class MainActivity extends AppCompatActivity {
 
