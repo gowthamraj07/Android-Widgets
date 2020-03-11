94da5a7851bbe9d79e7bf8b349a081a7e6e4e5f6
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2020-03-11 17:02:47 +0100

    Add BottomNavigationView with one child DashFormatterFragment

--- app/src/main/java/com/android/widgets/MainActivity.java
+++ app/src/main/java/com/android/widgets/MainActivity.java
@@ -3,6 +3,10 @@ package com.android.widgets;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpNavigation();
     }
 
-
+    public void setUpNavigation() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.btm_navigation);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
+    }
 }
