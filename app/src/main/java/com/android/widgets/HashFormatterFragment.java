7c7a3ce5ebbe92979c66d64e0151ac021b598225
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2020-03-13 08:12:23 +0100

    Add example for Hash Formatting

--- app/src/main/java/com/android/widgets/HashFormatterFragment.java
+++ app/src/main/java/com/android/widgets/HashFormatterFragment.java
@@ -5,14 +5,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.androidwidgets.formatedittext.formatter.HashFormatter;
+import com.androidwidgets.formatedittext.widgets.FormatEditText;
 
 public class HashFormatterFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_hash_formatter, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FormatEditText etDate = view.findViewById(R.id.et_date);
+        etDate.setFormatter(new HashFormatter("##-###-####"));
+        etDate.setHint("dd-MMM-yyyy");
     }
 }
