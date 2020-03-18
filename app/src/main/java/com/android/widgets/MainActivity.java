492fbe7155123b95e310bc1cf466a4f0fe4239af
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2020-03-18 11:23:15 +0100

    Replace bottom navigation with left drawer

--- app/src/main/java/com/android/widgets/MainActivity.java
+++ app/src/main/java/com/android/widgets/MainActivity.java
@@ -1,26 +1,52 @@
 package com.android.widgets;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
         setUpNavigation();
     }
 
     public void setUpNavigation() {
-        BottomNavigationView bottomNavigationView = findViewById(R.id.btm_navigation);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
+        NavigationUI.setupWithNavController(navigationView, navHostFragment.getNavController());
+
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_list);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (drawerLayout == null) {
+            return super.onOptionsItemSelected(item);
+        }
+
+        if (item.getItemId() == android.R.id.home) {
+            drawerLayout.openDrawer(Gravity.LEFT);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
